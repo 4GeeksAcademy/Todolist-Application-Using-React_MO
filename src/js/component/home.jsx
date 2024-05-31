fb6de20fde1db291d7883e5ae4216f9d4f9b05e3
MarOlivieri <141793518+MarOlivieri@users.noreply.github.com>
2024-05-31 19:14:06 +0000

    function create for delete todos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,42 +1,67 @@
 import React, { useState } from "react";
-// Create your first component
+
 const Home = () => {
     const [inputValue, setInputValue] = useState("");
     const [todos, setTodos] = useState([]);
+
     const handleKeyPress = (e) => {
-        if (e.key === 'Enter') {
+        if (e.key === 'Enter' && inputValue.trim() !== "") {
             setTodos([...todos, inputValue]);
             setInputValue("");
         }
     };
+
+    const deleteTodo = (indexToDelete) => {
+        setTodos(todos.filter((_, index) => index !== indexToDelete));
+    };
+
+    const resetTodos = () => {
+        setTodos([]);
+    };
+
     return (
         <div>
             <div className="container">
                 <h1 className="text-center mt-5">My ALLS ITEMS</h1>
-                <ul>
-                    <li>
-                        <input
-                            type="text"
-                            onChange={(e) => setInputValue(e.target.value)}
-                            value={inputValue}
-                            onKeyPress={handleKeyPress}
-                            placeholder="What do you need to do"
-                        />
+                <ul className="list-group">
+                    <li className="list-group-item">
+                        <div className="input-group">
+                            <input
+                                type="text"
+                                onChange={(e) => setInputValue(e.target.value)}
+                                value={inputValue}
+                                onKeyPress={handleKeyPress}
+                                placeholder="What do you need to do"
+                                className="form-control"
+                            />
+                        </div>
                     </li>
                     {todos.map((item, index) => (
-                        <li key={index}>
-                            {item} {" "}
-                            <i className="fa-solid fa-xmark" onClick={() => setTodos(todos.filter((_, i) => i !== index))} />
+                        <li key={index} className="list-group-item">
+                            <div className="input-group">
+                                <input
+                                    type="text"
+                                    value={item}
+                                    readOnly
+                                    className="form-control"
+                                />
+                                <div className="input-group-append">
+                                    <span
+                                        className="input-group-text"
+                                        onClick={() => deleteTodo(index)}
+                                        style={{ cursor: "pointer" }}
+                                    >
+                                       <i class="fa-solid fa-trash"></i>
+                                    </span>
+                                </div>
+                            </div>
                         </li>
                     ))}
-                    <li>Make the bed <i className="fa-solid fa-xmark" /></li>
-                    <li>Wash hands <i className="fa-solid fa-xmark" /></li>
-                    <li>Walk the dog <i className="fa-solid fa-xmark" /></li>
-                    <li>EAT! <i className="fa-solid fa-xmark" /></li>
                 </ul>
+               
             </div>
-            <div>20 tasks</div>
         </div>
     );
 };
+
 export default Home;
